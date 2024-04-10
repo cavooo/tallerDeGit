2c8473d1e1f659d4216b05a0fdd9ba0f21a9d18e
franco <francolacavab@gmail.com>
2024-04-10 09:58:12 -0300

    empieza practica

--- guia3.hs
+++ guia3.hs
@@ -1,4 +1,5 @@
 -- ej 4
+{-el _ se usa como comodin, 
 
 probelma todoMenor (p: RxR, q:R,R): Bool {
     requiere {True}
@@ -6,4 +7,13 @@ probelma todoMenor (p: RxR, q:R,R): Bool {
 }
 
 
--- el _ se usa como comodin, 
+
+Float es real, integer es enteros!!
+
+-}
+
+-- patter mathching matchea (a,b) (c,d) con (Float,Float) (Float, Float)
+-- todoMenor (a,b) (c,d) = a > c && b > d = True
+
+todoMenor :: (Float, Float) -> (Float, Float) -> Bool
+todoMenor p q = fst p > fst c && snd b > snd d = True
