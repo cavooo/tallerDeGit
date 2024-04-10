d0867394c42534fb57786a2fc59a6ddcabff4a53
franco <francolacavab@gmail.com>
2024-04-10 10:26:26 -0300

    anteultimo

--- guia3.hs
+++ guia3.hs
@@ -1,19 +1,27 @@
--- ej 4
+todoMenor :: (Float, Float) -> (Float, Float) -> Bool
+todoMenor (a,b) (c,d) = a > c && b > d
+
+
+
+
 {-el _ se usa como comodin, 
-
-probelma todoMenor (p: RxR, q:R,R): Bool {
-    requiere {True}
-    asegura {res = True si p_0 < q_0 y p1 < q_1, y sino res = False}
-}
-
-
 
 Float es real, integer es enteros!!
 
+patter mathching matchea (a,b) (c,d) con (Float,Float) (Float, Float)
+todoMenor (a,b) (c,d) = a > c && b > d = True
+
+posPrimerPar: dada una terna de enteros, devuelve la posici´on del primer n´umero par si es que hay alguno, y devuelve
+4 si son todos impares
+
 -}
 
--- patter mathching matchea (a,b) (c,d) con (Float,Float) (Float, Float)
--- todoMenor (a,b) (c,d) = a > c && b > d = True
+posPrimerPar :: (Int, Int) -> (Int, Int) -> Int
+posPrimerPar (a,b) (c,d)
+    |a mod 2 == 0 = 1
+    |b mod 2 == 0 = 1
+    |c mod 2 == 0 = 1
+    |d mod 2 == 0 = 1
+    |otherwise = 4
 
-todoMenor :: (Float, Float) -> (Float, Float) -> Bool
-todoMenor p q = fst p > fst c && snd b > snd d = True
+
